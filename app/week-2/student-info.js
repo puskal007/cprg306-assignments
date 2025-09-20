8c8967c3f359699dcc7328067841bdffaff16567
Puskal <PuskalRaj.Lamsal@edu.sait.ca>
2025-09-19 20:42:20 -0600

    Initial commit with Next.js project for week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Puskal Lamsal</p>
+      <Link href="https://github.com/puskal007">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+}
