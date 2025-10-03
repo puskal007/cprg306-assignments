7cd8853feb7a406e86b567608971288ab88a7660
Puskal <PuskalRaj.Lamsal@edu.sait.ca>
2025-10-03 16:49:28 -0600

    Completed Week 3 assignment - Shopping List with props

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,7 +1,7 @@
 
 import Item from './item';
 
-export default function ItemList() 
+export default function ItemList() {
   const item1 = {
   name: "milk, 4 L 🥛",
   quantity: 1,
@@ -73,3 +73,22 @@ const item12 = {
   quantity: 4,
   category: "household",
 };
+
+
+ return (
+  <ul>
+    <Item name={item1.name} quantity={item1.quantity} category={item1.category} />
+    <Item name={item2.name} quantity={item2.quantity} category={item2.category} />
+    <Item name={item3.name} quantity={item3.quantity} category={item3.category} />
+    <Item name={item4.name} quantity={item4.quantity} category={item4.category} />
+    <Item name={item5.name} quantity={item5.quantity} category={item5.category} />
+    <Item name={item6.name} quantity={item6.quantity} category={item6.category} />
+    <Item name={item7.name} quantity={item7.quantity} category={item7.category} />
+    <Item name={item8.name} quantity={item8.quantity} category={item8.category} />
+    <Item name={item9.name} quantity={item9.quantity} category={item9.category} />
+    <Item name={item10.name} quantity={item10.quantity} category={item10.category} />
+    <Item name={item11.name} quantity={item11.quantity} category={item11.category} />
+    <Item name={item12.name} quantity={item12.quantity} category={item12.category} />
+  </ul>
+);
+}
