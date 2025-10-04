8359af97baa20535d556b7217c7ebed7255dfc30
Puskal <PuskalRaj.Lamsal@edu.sait.ca>
2025-10-03 20:30:07 -0600

    Completed week 2 assignment

--- app/page.js
+++ app/page.js
@@ -15,6 +15,11 @@ export default function Home() {
             Week 3 Assignment
           </Link>
         </li>
+        <li>
+          <Link href="/week-4" className="text-blue-600 hover:text-blue-800 underline">
+            Week 4 Assignment
+          </Link>
+        </li>
       </ul>
     </main>
   );
