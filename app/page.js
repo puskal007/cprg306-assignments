05be9c4d19e0c76a5248d94c8e6cab8d58a88b1b
Puskal <PuskalRaj.Lamsal@edu.sait.ca>
2025-10-24 10:55:42 -0600

    Completed Week 7 assignment - Integrated shopping list application

--- app/page.js
+++ app/page.js
@@ -30,6 +30,11 @@ export default function Home() {
             Week 6 Assignment
           </Link>
         </li>
+        <li>
+          <Link href="/week-7" className="text-blue-600 hover:text-blue-800 underline">
+            Week 7 Assignment
+         </Link>
+        </li>
       </ul>
     </main>
   );
